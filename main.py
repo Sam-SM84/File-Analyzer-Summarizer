afb4f9680d1d74f90d95492d4016df202910cd38
Sam-SM84 <sam.salahimoghadam@gmail.com>
2026-08-10 19:29:54 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from analyzer import extract_keywords , describtion
+from analyzer import print_keywords , describtion
 from summarizer import summarize
 from file_reader import load_PDF , load_text
 
 import sys
 sys.stdout.reconfigure(encoding="utf-8")
 
-text , file = loadPDF("papers/OpenGL-cutted.pdf")
+
+text , file = load_PDF("papers/Joz2.pdf")
+
 print("------------- Original Text -------------")
 describtion(text,file)
+
 print("------------- Extracted Words -------------")
-#analyse(cleanse(text,False),file)
-keywords = extract_keywords(text,15)
-for word , count in keywords:
-    print(f"{word} : {count}")
+print_keywords(text,5,english=False)
+
 
 print("------------- PDF summary -------------")
-summarized_text = summarize(text,sentence_count=3)
+summarized_text = summarize(text,paragraph_count=1)
 print(summarized_text)
+
+
