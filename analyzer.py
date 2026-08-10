f32fb62896f9ace5b26156a3ad1f515ef431ea08
Sam-SM84 <sam.salahimoghadam@gmail.com>
2026-08-10 19:30:27 +0330

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,21 +1,41 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from collections import Counter
+from hazm import *
 
-def extract_keywords(text,n):
-    tokens = word_tokenize(text)
-    stop_words = set(stopwords.words('english'))
-    #tokens = cleanse(text)
-    cleaned = []
-    symbols = ['(',')',',','.','-','/','_']
-    for word in tokens:
-        if (word.lower() not in stop_words and
-            word not in symbols and
-            len(word) > 1 and
-            word.isalpha()):
-            cleaned.append(word)
+def print_keywords(text,n,english=True):
+    if english : 
+        tokens = word_tokenize(text)
+        
+        stop_words = set(stopwords.words('english'))
+        #tokens = cleanse(text)
+        cleaned = []
+        symbols = ['(',')',',','.','-','/','_']
+        for word in tokens:
+            if (word.lower() not in stop_words and
+                word not in symbols and
+                len(word) > 1 and
+                word.isalpha()):
+                cleaned.append(word)
 
-    return Counter(cleaned).most_common(n)
+        keywords =  Counter(cleaned).most_common(n)
+        for word , count in keywords:
+            print(f"{word} : {count}")
+
+    else :
+        n = Normalizer()
+        text = n.normalize(text)
+        tokens = word_tokenize(text)
+        stop_words = set(stopwords_list())
+
+        cleaned = []
+        symbols = ['(',')',',','.','-','/','_']
+        for kalame in tokens:
+            if (kalame not in stop_words and
+                kalame not in symbols and
+                len(kalame) > 1) :
+                cleaned.append(kalame)
+        print(cleaned)
 
 def describtion(text,file):
     print("Text Describtion : ")
