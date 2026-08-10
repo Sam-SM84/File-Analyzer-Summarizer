3853bca62effabbcb5ac96ef6bc0a3b6b2278120
Sam-SM84 <sam.salahimoghadam@gmail.com>
2026-08-10 12:59:03 +0330

    Update file_reader.py

--- file_reader.py
+++ file_reader.py
@@ -1,9 +1,15 @@
 import pymupdf
 
-def loadPDF(path):
+def load_PDF(path):
     text = ""
     file = pymupdf.open(path)
     
     for page in file:
         text += page.get_text()
     return text , file
+
+def load_text(path):
+    file = open(path,'r')
+    text = file.read()
+    file.close()
+    return text
