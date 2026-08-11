0fd16091fa89deee1c076faf159886a099ccc726
Sam-SM84 <sam.salahimoghadam@gmail.com>
2026-08-11 18:58:30 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from analyzer import print_keywords , describtion
+from analyzer import *
 from summarizer import summarize
-from file_reader import load_PDF , load_text
+from file_reader import *
 
 import sys
 sys.stdout.reconfigure(encoding="utf-8")
 
 
-text , file = load_PDF("papers/Joz2.pdf")
+text_list = load_PDF("papers/Joz2.pdf")
+
 
 print("------------- Original Text -------------")
-describtion(text,file)
+describtion(text_list)
 
 print("------------- Extracted Words -------------")
-print_keywords(text,5,english=False)
-
+print_keywords(return_string(text_list),5)
 
 print("------------- PDF summary -------------")
-summarized_text = summarize(text,paragraph_count=1)
+summarized_text = summarize(return_string(text_list),paragraph_count=3)
 print(summarized_text)
 
-
+print(search_text(text_list,'برنامه',10))
