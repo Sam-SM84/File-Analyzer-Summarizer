2722e28458a10491969f71bf3f0f09e29748774f
Sam-SM84 <sam.salahimoghadam@gmail.com>
2026-08-13 18:09:37 +0330

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -3,14 +3,16 @@ from nltk.tokenize import word_tokenize
 from collections import Counter
 from hazm import *
 from langdetect import detect
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def return_keywords(text,n):
     english = detect(text) == 'en'
     message = []
     if english : 
         tokens = word_tokenize(text)
-        
-        stop_words = set(stopwords.words('english'))
+
+        """
+                stop_words = set(stopwords.words('english'))
         cleaned = []
         symbols = ['(',')',',','.','-','/','_']
         for word in tokens:
@@ -19,7 +21,8 @@ def return_keywords(text,n):
                 len(word) > 1 and
                 word.isalpha()):
                 cleaned.append(word)
-
+        """
+        cleaned = clean_list_english(tokens)
         keywords =  Counter(cleaned).most_common(n)
         for word , count in keywords:
             message.append({"Word" : word,"Count" : count}) 
@@ -28,7 +31,8 @@ def return_keywords(text,n):
         normalizer = Normalizer()
         text = normalizer.normalize(text)
         tokens = word_tokenize(text)
-        stop_words = set(stopwords_list())
+        """
+                stop_words = set(stopwords_list())
 
         cleaned = []
         symbols = ['(',')',',','.','-','/','_']
@@ -37,11 +41,32 @@ def return_keywords(text,n):
                 kalame not in symbols and
                 len(kalame) > 1) :
                 cleaned.append(kalame)
+        """
+        tamiz = clean_list_persian(tokens)
 
-        keywords =  Counter(cleaned).most_common(n)
+        keywords =  Counter(tamiz).most_common(n)
         for word , count in keywords:
             message.append({"Word" : word,"Count" : count}) 
 
+    return message
+
+def return_TFIDF(text_list,n):
+    english = detect(return_string(text_list)) == 'en'
+    if english : 
+        text_list = clean_list_english(text_list)
+    else :
+        text_list = clean_list_persian(text_list)
+
+    tfidf = TfidfVectorizer()
+    result = tfidf.fit_transform(text_list)
+    words = tfidf.get_feature_names_out()
+    scores = result.mean(axis=0).A1
+    keywords = list(zip(words,scores))
+    keywords.sort(key=lambda x:x[1],reverse=True)
+
+    message = []
+    for word , score in keywords[:n]:
+        message.append({"Word" : word,"Score" : score})
     return message
 
 def describtion(text):
@@ -59,3 +84,25 @@ def return_string(text_list):
         text_string += page
     return text_string
 
+def clean_list_english(text_list):
+    stop_words = set(stopwords.words('english'))
+    cleaned = []
+    symbols = ['(',')',',','.','-','/','_']
+    for word in text_list:
+        if (word.lower() not in stop_words and
+            word not in symbols and
+            len(word) > 1 and
+            word.isalpha()):
+            cleaned.append(word)
+    return cleaned
+
+def clean_list_persian(text_list):
+    stop_words = set(stopwords_list())
+    tamiz = []
+    symbols = ['(',')',',','.','-','/','_']
+    for kalame in text_list:
+        if (kalame not in stop_words and
+            kalame not in symbols and
+            len(kalame) > 1) :
+            tamiz.append(kalame)
+    return tamiz
