8859c126125585a88546773e8c60cc0f78998453
Sam-SM84 <sam.salahimoghadam@gmail.com>
2026-08-13 12:43:29 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ sys.stdout.reconfigure(encoding="utf-8")
 
 import streamlit as st
 
+import pandas as pd
+
 #python -m streamlit run F:\my_python\paperAnalyzer\main.py
 
 st.title("File Analyzer/Summarizer")
-
+st.subheader("by Sam SalahiMoghaddam")
 uploaded_file = st.file_uploader(
     "Upload a file",
     accept_multiple_files=False,
     type=["pdf","txt"]
 )
 text_list = []
+output = []
 
 if uploaded_file:
     st.success(f"Successfuly uploaded . Selected file : {uploaded_file.name}")
@@ -29,33 +32,52 @@ method = st.selectbox("What should this program do? : ",['Extract keywords','Fin
 if method == 'Extract keywords':
     #st.write(return_string(text_list))
     n = st.number_input("Enter number of top common words :", min_value=5, format="%d")
-    if st.button("Print"):
+    if st.button("Show results") : 
+        output.clear()
+        output.append("-------------------------")
+        output.append("Top " + str(n) + " repeated words : ")
+            #st.write("Top " + str(n) + " repeated words : ")
         keywords = return_keywords(return_string(text_list),n)
         for i in keywords:
-            st.write(i)
+            output.append(str(i['Word']) + " : " + str(i['Count']))
+                #st.write(i)
+        output.append("-------------------------")
+
+        df = pd.DataFrame(keywords)
+        chart = st.bar_chart(df, x="Word", y="Count")
+        
 
 elif method == 'Find word':
     word = st.text_input("Enter the word : ")
+    output.clear()
+    output.append("Results for the search of (" + word + ")")
     distance = st.slider("Choose the distance level of the word displayment : ",min_value=5,max_value=100)
-    if st.button("search") :
+    if st.button("Show results") : 
         if word:
             result = search_text(text_list,word,distance)
             for r in result:
-                st.write("-------------------------")
-                st.write("Page : ",r['Page'] + 1)
-                st.write("Text : ",r['Text'])
+                output.append("-------------------------")
+                output.append("Page : " + str(r['Page'] + 1))
+                output.append("Text : " + str(r['Text']))
 
         else :
             st.error("Please fill the input box")
 
 elif method == 'Summarize':
     count = st.slider("Choose the paragraph amount of the summarized text : ",min_value=1,max_value=7)
-    if st.button("summarize") :
-        st.write(summarize(return_string(text_list),count))
+    if st.button("Show results") : 
+        output.clear()
+        output.append(summarize(return_string(text_list),count))
 
 else:
-    desc = describtion(text_list)
-    for i in desc : 
-        st.write(i)
+    if st.button("Show results") : 
+        desc = describtion(text_list)
+        output.clear()
+        for i in desc : 
+            output.append(i)
+        output.append("-------------------------")
+
+for result in output:
+    st.write(result)
 
 
